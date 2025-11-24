82f5ded6b002ed745df7d177e11e32d1118a9506
Yan Menephyl <87574460+Menephyl@users.noreply.github.com>
2025-11-24 09:02:06 -0300

    upload the files

---
+++ ebookDownload/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import ThankYouPage from './components/thanksPage.js';
+function App() {
+ 
+
+  return (
+    <>
+      <ThankYouPage />
+    </>
+  )
+}
+
+export default App
